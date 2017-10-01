cdb79192ffc4b48c02e307b73b4be46e8ff4dc53
eltos <eltos@outlook.de>
2017-10-01 13:04:42 +0200

    Release

--- make.cmd
+++
@@ -1,6 +0,0 @@
-pyinstaller --windowed ^
-	--distpath build ^
-	--workpath build/tmp ^
-	--icon=D.ico ^
-	--add-data D.ico;. --add-data D.png;. --add-data themes;themes --add-data translations;translations ^
-	-F durak.pyw
--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-pyinstaller --windowed \
-	--distpath build \
-	--workpath build/tmp \
-	--icon=D.ico \
-	--add-data D.ico:. --add-data D.png:. --add-data themes:themes --add-data translations:translations \
-	-n Durak_$(uname)_$(arch)\
-	-F durak.pyw
